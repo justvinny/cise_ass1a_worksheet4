089d4c9e0a7c7fd4cc91348721058d1bf9248617
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-04-02 17:15:48 +1300

    added submit article form

--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SubmissionForm from "../components/SubmissionForm";
 
 const SubmitArticle = () => {
   return (
@@ -8,16 +9,10 @@ const SubmitArticle = () => {
         This will be a form to allow submitters to submit an article for
         possible inclusion in our SEPER repo.
       </p>
-      <p>
-        Also the display table of evidence levels for different claims will show
-        here for the SE practice selected
-      </p>
-      <p>
-        Also the display table of evidence levels for different claims will show
-        here for the SE practice selected
-      </p>
+      <SubmissionForm />
     </div>
   );
 };
+
 
 export default SubmitArticle;
