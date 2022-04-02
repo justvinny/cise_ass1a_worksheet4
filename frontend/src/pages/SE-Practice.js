7baf96fbf3b6f53d539e0d6a5bd98d7497d41e1f
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-04-02 13:44:30 +1300

    first commit

---
+++ frontend/src/pages/SE-Practice.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SEPractice = () => {
+  return (
+    <div>SEPractice</div>
+  )
+}
+
+export default SEPractice;
---
+++ frontend/src/pages/Submit-Article.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SubmitArticle = () => {
+  return (
+    <div>SubmitArticle</div>
+  )
+}
+
+export default SubmitArticle;
