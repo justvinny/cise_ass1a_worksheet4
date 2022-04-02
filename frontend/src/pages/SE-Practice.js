3595b342877e091b2abd045179f99aceec5f846d
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-04-02 15:36:22 +1300

    added dropdown

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -2,11 +2,13 @@ import articles from "../dummydata/articles";
 import TableStyles from "../components/TableStyle";
 import Table from "../components/EvidenceTable";
 import TableColumns from "../components/TableColumns";
+import Dropdown from "../components/Dropdown";
 
 const SEPractice = () => {
   return (
     <div>
       <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+      <Dropdown />
       <TableStyles>
         <Table data={articles} columns={TableColumns} />
       </TableStyles>
